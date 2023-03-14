1b51c85ae21c4d60b04c4d01c04cb991e76f8568
zahrabateninia <zahrabatenin@gmail.com>
2023-03-14 17:17:15 +0330

    the comments are improved and some rationl buggs are fixed

--- game.js
+++ game.js
@@ -1,11 +1,9 @@
-//create a function to return a random choice as the computer choice using Math.random()
-
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors']; //they should be stings so I quoted them
+    const choices = ['rock', 'paper', 'scissors']; 
     
-    let choiceIndex = getRandomInt(0,3); //returns an integer between 0 and 2
+    let choiceIndex = getRandomInt(0,3); 
     //console.log(choiceIndex);
-    let computerChoice =  choices[choiceIndex]; //local , cannot be used out side of this scope!
+    let computerChoice =  choices[choiceIndex]; 
     return(computerChoice); 
     
 }
@@ -17,8 +15,6 @@ function getRandomInt(min, max) {
 }
 //***************************** 
 
-//player selection 
-
 function playerSelection() {
     let playerChoice = prompt('Rock or paper or scissors?');
     //case insensitivity 
@@ -27,36 +23,37 @@ function playerSelection() {
 }
 
 //*********************************
-
-// checks who wins/loses, the user or the computer
-//need a counter to count the times they win
-
-let computerCount = 0;
-let playerCount = 0; 
+let computerCount = 1;
+let playerCount = 1; 
 
 function playRound(player, computer){
     console.log('You chose '+player+ ' and the computer chose ' +computer+'\n' +'As a result: ');
-    if(player === computer ){ //whenever you wanna use a function you must call it with parenthesis
-        //console.log(computerCount++);
-        console.log(playerCount++);
-        return("it's a tie!") ;   
+    if(player === computer ){ 
+        return("It's a tie!") ;  
+
     }else if ((player=== 'rock' && computer === 'paper') || (player==='paper' && computer==='scissors') 
     || (player==='scissors' && computer==='rock')) { 
-        //console.log(computerCount++);
-        return('computer wins.'); 
+        console.log("The computer's point:" + computerCount++)
+        return('The computer wins.'); 
      }else{
-        console.log(playerCount++);
-        return('you won!');
+        console.log('Your point:' + playerCount++);
+        return('You win!');
      }
     
     }   
 
 //******************************************
 //if the computer or the player reached 5 times wining stop the game 
-while(true){// there are infinite possibilities
+
+while(true){
+
     console.log(playRound(playerSelection(), getComputerChoice())); 
     
-    if ((computerCount === 6) || (playerCount === 6) ){
+    if (computerCount === 6){
+        console.log('The computer won the game!');
+        break;
+    }else if (playerCount === 6){
+        console.log('You beat the computer five times so you won! bravo.');
         break;
     }
 }
